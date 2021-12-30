5e261ea595f883fc8ab3438ef0e2671ceb8540ba
Juan Pablo Vanegas <jpvanegasc@gmail.com>
2021-12-29 19:16:50 -0500

    :sparkles: Add local logger

---
+++ api/common/__init__.py
@@ -0,0 +1,1 @@
+from api.common.settings import settings
--- api/main.py
+++ api/main.py
@@ -1,12 +1,24 @@
+import logging
+from logging.config import dictConfig
+
 from fastapi import FastAPI
 from mangum import Mangum
 
+from api.common.settings import LogConfig
+
+dictConfig(LogConfig().dict())
+
 app = FastAPI()
+logger = logging.getLogger("logger")
 
 
 @app.get("/")
 def hello_world():
-    return {"data": "hello world!"}
+    logger.info("Test Info")
+    logger.error("Test Error")
+    logger.debug("Test Debug")
+    logger.warning("Test Warning")
+    return {"data": f"hello world!"}
 
 
 handler = Mangum(app)
