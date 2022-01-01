3e21109aa6029265901176756de3f2f0948a0420
Juan Pablo Vanegas <jpvanegasc@gmail.com>
2022-01-01 11:40:53 -0500

    :sparkles: Use app factory

--- api/main.py
+++ api/main.py
@@ -2,31 +2,32 @@ import logging
 from logging.config import dictConfig
 
 from mangum import Mangum
-from fastapi import FastAPI, Depends, Request
+from fastapi import FastAPI, Request
 from fastapi.responses import JSONResponse
 
-from api.common import db_session, MessageException
+from api.common import MessageException
 from api.common.settings import LogConfig
 from api.routers.user import router as user_router
 
 dictConfig(LogConfig().dict())
-
-app = FastAPI()
 logger = logging.getLogger("logger")
 
 
-@app.exception_handler(MessageException)
-async def message_exception_handler(request: Request, exc: MessageException):
-    return JSONResponse(status_code=exc.status_code, content=exc.to_dict())
+def include_routers(app):
+    app.include_router(user_router)
 
 
-app.include_router(user_router)
+def start_app():
+    app = FastAPI()
+
+    @app.exception_handler(MessageException)
+    async def message_exception_handler(request: Request, exc: MessageException):
+        return JSONResponse(status_code=exc.status_code, content=exc.to_dict())
+
+    include_routers(app)
+
+    return app
 
 
-@app.get("/")
-def hello_world(db=Depends(db_session)):
-    query = db.execute("SELECT 1").first()
-    return {"data": f"hello world! DB says: {query}"}
-
-
+app = start_app()
 handler = Mangum(app)
