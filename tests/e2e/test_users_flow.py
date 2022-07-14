fc9161938a072a025d81549e970ff438e47cd887
Juan Pablo Vanegas <jpvanegasc@gmail.com>
2022-07-14 15:40:13 -0500

    :white_check_mark: Add more cases to e2e tests

--- tests/e2e/test_users_flow.py
+++ tests/e2e/test_users_flow.py
@@ -1,4 +1,6 @@
-from tests.utils import mock_load, assert_code, assert_data
+from uuid import uuid4
+
+from tests.utils import mock_load, assert_code, assert_data, assert_message
 
 
 def test_successful_user_flow(client):
@@ -16,6 +18,12 @@ def test_successful_user_flow(client):
     assert_code(response, 201)
     assert_data(UserBase, response, data)
 
+    # Create duplicate
+    response = client.post("api/users", json=data)
+
+    assert_code(response, 400)
+    assert_message(response, "user already exists")
+
     # Read many
     response = client.get("api/users")
 
@@ -28,6 +36,12 @@ def test_successful_user_flow(client):
     assert_code(response, 200)
     assert_data(UserBase, response, data)
 
+    # Read one - not found
+    response = client.get(f"api/users/{uuid4()}")
+
+    assert_code(response, 404)
+    assert_message(response, "user not found")
+
     # Update
     new_username = "new_username"
     data["username"] = new_username
@@ -37,9 +51,20 @@ def test_successful_user_flow(client):
     assert_code(response, 200)
     assert_data(UserBase, response, data)
 
+    # Update - not found
+    response = client.patch(f"api/users/{uuid4()}", json={"username": ""})
+
+    assert_code(response, 404)
+    assert_message(response, "user not found")
+
     # Delete
     response = client.delete(f"api/users/{user_id}")
     get_response = client.get(f"api/users/{user_id}")
 
     assert_code(response, 200)
     assert_code(get_response, 404)
+
+    # Delete - not found
+    response = client.delete(f"api/users/{uuid4()}")
+
+    assert_code(response, 404)
