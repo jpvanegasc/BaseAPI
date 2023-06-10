b5adf0dc0bcdda618818a370df1b869c62d647e1
Juan Pablo Vanegas <jpvanegasc@gmail.com>
2023-06-10 17:24:27 -0500

    :sparkles: Working user routers

--- app/routers/user.py
+++ app/routers/user.py
@@ -2,52 +2,80 @@ from fastapi import APIRouter, Depends, status
 from fastapi.responses import JSONResponse
 
 from app.database import db_session
-from app.exceptions import UserNotFound, DuplicateUser
-import app.services as services
+import app.services.user as user_service
 import app.schemas.user as user_schemas
+from app.dependencies.user import valid_user
 
 router = APIRouter(prefix="/api/users", tags=["Users"])
 
 
 @router.post(
     "",
-    # TODO: add more details
+    response_model=user_schemas.UserRead,
+    responses={
+        status.HTTP_201_CREATED: {
+            "model": user_schemas.UserRead,
+            "description": "Success",
+        },
+        status.HTTP_400_BAD_REQUEST: {"description": "Duplicate user"},
+    },
+    status_code=status.HTTP_201_CREATED,
+    summary="Create a new user",
 )
-def create_user(new_user: user_schemas.UserCreate, db=Depends(db_session)):
-    existing = services.user.get_by_username(db, new_user.username)
-
-    if existing:
-        raise DuplicateUser()
-
-    return services.user.create(db, new_user.dict())
+def create_user(
+    new_user: user_schemas.UserCreate,
+    db=Depends(db_session),
+):
+    return user_service.create_user(db, new_user)
 
 
-@router.get("")
-def get_all_users(db=Depends(db_session)):
-    return services.user.all(db)
+@router.get(
+    "",
+    response_model=user_schemas.UserPagedResponse,
+    responses={
+        status.HTTP_200_OK: {
+            "model": user_schemas.UserPagedResponse,
+            "description": "Success",
+        },
+    },
+    status_code=status.HTTP_200_OK,
+    summary="Get all users",
+)
+def get_all_users(page: int = 1, per_page: int = 10, db=Depends(db_session)):
+    users = user_service.get_all_users(db, page_number=page, per_page=per_page)
+    user_count = user_service.user_count(db)
+    return user_schemas.UserPagedResponse(items=users, total=user_count)
 
 
-@router.get("/{user_id}")
-def get_by_id(user_id, db=Depends(db_session)):
+@router.get(
+    "/{user_id}",
+    response_model=user_schemas.UserRead,
+    responses={
+        status.HTTP_200_OK: {
+            "model": user_schemas.UserRead,
+            "description": "Success",
+        },
+        status.HTTP_404_NOT_FOUND: {"description": "User not found"},
+    },
+    status_code=status.HTTP_200_OK,
+    summary="Get a user",
+)
+def get_by_id(user: user_schemas.UserRead = Depends(valid_user)):
+    return user_schemas.UserRead.from_orm(user)
 
-    if not (user := services.user.get_by_id(db, user_id)):
-        raise UserNotFound()
 
-    return user
-
-
-@router.patch("/{user_id}")
-def edit_user(user_id, update_data: user_schemas.UserUpdate, db=Depends(db_session)):
-    if not services.user.get_by_id(db, user_id):
-        raise UserNotFound()
-
-    return services.user.update(db, user_id, update_data.dict())
-
-
-@router.delete("/{user_id}")
-def delete_user(user_id, db=Depends(db_session)):
-    if not services.user.get_by_id(db, user_id):
-        raise UserNotFound()
-
-    services.user.delete(db, user_id)
+@router.delete(
+    "/{user_id}",
+    responses={
+        status.HTTP_202_ACCEPTED: {"description": "Success"},
+        status.HTTP_404_NOT_FOUND: {"description": "User not found"},
+    },
+    status_code=status.HTTP_202_ACCEPTED,
+    summary="Delete a user",
+)
+def delete_user(
+    user=Depends(valid_user),
+    db=Depends(db_session),
+):
+    user_service.delete_user(db, user)
     return JSONResponse(status_code=status.HTTP_202_ACCEPTED, content="user deleted")
