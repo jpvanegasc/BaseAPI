16fbb490c2fd1aa67a04787a8f1163e033a6e6f8
Juan Pablo Vanegas <jpvanegasc@gmail.com>
2023-06-10 17:19:18 -0500

    :sparkles: Improve exception handling

--- app/exceptions.py
+++ app/exceptions.py
@@ -1,6 +1,19 @@
-class UserNotFound(Exception):
-    pass
+from fastapi import HTTPException, status
 
 
-class DuplicateUser(Exception):
-    pass
+class DetailException(HTTPException):
+    status_code = status.HTTP_400_BAD_REQUEST
+    detail = None
+    headers = None
+
+    def __init__(self) -> None:
+        super().__init__(self.status_code, self.detail, self.headers)
+
+
+class UserNotFound(DetailException):
+    status_code = 404
+    detail = "user not found"
+
+
+class DuplicateUser(DetailException):
+    detail = "duplicate user"
